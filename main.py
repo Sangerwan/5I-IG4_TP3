9da05dbd926c3e82110602438f65abe95b8b21ac
Sangerwan <sangerwan@gmail.com>
2022-10-19 11:17:41 +0200

    img

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import numpy as np
 import glob
 import sklearn
 import sklearn.cluster
+import matplotlib.pyplot as plt
 #128*nbpts
 
-def vocabulaire(N, chemins, fichier, methode = "kmeans"):
+def vocabulaire(N, chemins, fichier=None, methode = "kmeans"):
     s=cv2.SIFT_create()
     listdesc = []
     for chemin in chemins:
         for image in glob.glob(chemin):
-            print(image)
+            #print(image)
             img=cv2.imread(image)
             kpt, descriptor = s.detectAndCompute(img, None)
             listdesc.append(descriptor) 
@@ -21,6 +22,7 @@ def vocabulaire(N, chemins, fichier, methode = "kmeans"):
         kmeans.cluster_centers_
         kmeans.labels_
         max = 0
+
         for desc in listdesc:
             try:
                 label = kmeans.fit_predict(desc)
@@ -29,15 +31,26 @@ def vocabulaire(N, chemins, fichier, methode = "kmeans"):
                 continue
             dist = np.linalg.norm(kmeans.cluster_centers_[label]-desc)
             if dist > max:
-
                 max = dist
     if fichier is not None:
         np.savetxt(fichier, kmeans.cluster_centers_, delimiter=',')
 
     return kmeans.inertia_/N, max
 
+def coude():
+    N = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,75,100,150,200,250,300,400,500,1000,1500,2000]
+    inertie = []
+    max = []
+    for n in N:
+        chemins = ["Img/pigeon/test/*.jpg", "Img/pizza/test/*.jpg", "Img/scorpion/test/*.jpg", "Img/sunflower/test/*.jpg"]
+        inertia, max_dist = vocabulaire(n, chemins)
+        inertie.append(inertia)
+        max.append(max_dist)
+    plt.figure(1)
+    plt.plot(N, inertie)
+    plt.figure(2)
+    plt.plot(N, max)
+    plt.show()
+    input("Press Enter to continue...")
 
-N = 100
-chemins = ["Img/pigeon/*.jpg", "Img/pizza/*.jpg", "Img/scorpion/*.jpg", "Img/sunflower/*.jpg"]
-fichier = "vocabulaire.txt"
-print(vocabulaire(N, chemins, fichier))
+coude()
